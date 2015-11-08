0902dd802e6a424bed8fe3cfaf2f2abc50d496b1
K43GFX <k43gfx@gmail.com>
2015-11-08 02:04:43 +0200

    Adding syslog-ng and install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+yum install syslog-ng
+yum install php
+yum install php-sqlite3
+yum install freeradius-utils
+mv radius /var
+chmod -R a+x /var/radius/
+rm -rf /etc/syslog-ng/syslog-ng.conf
+mv syslog-ng.conf /etc/syslog-ng/
+mkdir /mnt/ramdisk
+mount -t tmpfs -o size=256m tmpfs /mnt/ramdisk
+echo -e "tmpfs       /mnt/ramdisk tmpfs   nodev,nosuid,noexec,nodiratime,size=256m   0 0\n" >> /etc/fstab
+service syslog-ng restart
