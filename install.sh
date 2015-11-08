1c165868ed6c90071dae0ccd616f9feed603dc03
K43GFX <k43gfx@gmail.com>
2015-11-08 02:08:45 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@ yum install syslog-ng
 yum install php
 yum install php-sqlite3
 yum install freeradius-utils
+rm -rf /var/radius/
 mv radius /var
 chmod -R a+x /var/radius/
 rm -rf /etc/syslog-ng/syslog-ng.conf
-mv syslog-ng.conf /etc/syslog-ng/
+mv syslog-ng/syslog-ng.conf /etc/syslog-ng/
 mkdir /mnt/ramdisk
 mount -t tmpfs -o size=256m tmpfs /mnt/ramdisk
 echo -e "tmpfs       /mnt/ramdisk tmpfs   nodev,nosuid,noexec,nodiratime,size=256m   0 0\n" >> /etc/fstab
